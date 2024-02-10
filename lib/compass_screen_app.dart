6476637decca5c81949584f040280cdd9696783d
Антон Кулик <anton.smile.1998@gmail.com>
2024-02-10 18:09:35 +0300

    Создан экран под компас

---
+++ lib/compass_screen_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompassScreenApp extends StatefulWidget {
+  const CompassScreenApp({super.key});
+
+  @override
+  State<CompassScreenApp> createState() => _CompassScreenAppState();
+}
+
+class _CompassScreenAppState extends State<CompassScreenApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/shapes_screen_app.dart';
+// import 'package:flutter_application_2/shapes_screen_app.dart';
+import 'package:flutter_application_2/compass_screen_app.dart';
 
 void main() {
-  runApp(const ShapesScreenApp());
+  runApp(const CompassScreenApp());
 }
